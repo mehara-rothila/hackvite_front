4e79814a05e43e0ff6ee79e0b6b4eff75c2edca6
Mehara Rothila Ranawaka <rothilamehara22@gmail.com>
2025-07-31 23:48:36 +0530

    Updated Landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 export default function LandingPage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
